d79c3803a88743cd781a36a4fb1e4dd6c9e95a77
yangkun121 <2417403225@qq.com>
2021-11-15 14:46:29 +0800

    使用quartz动态创建修改定时任务

---
+++ springboot-quartz/src/main/java/com/huaiwei/springbootquartz/SpringbootQuartzApplication.java
@@ -0,0 +1,14 @@
+package com.huaiwei.springbootquartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SpringbootQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootQuartzApplication.class, args);
+    }
+
+}
